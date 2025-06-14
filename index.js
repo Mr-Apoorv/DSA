a33d90ad705513255ff92ed9e485f8b842bf001f
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-14 17:28:53 +0530

    NamasteDSA #M1L6 Q1 Find second largest number in an array, added solution for corner cases.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Write a function that returns the second largest number in an array.
 
 function secondLargest(arr) {
+  // If array is empty
+  // if array has less than two elements
+  if (arr.length < 2) {
+    console.log("Array must have at least two elements.");
+    return "Array must have at least two elements.";
+  }
+
   let largest = -Infinity;
   let secondLarge = -Infinity;
 
@@ -8,7 +15,10 @@ function secondLargest(arr) {
     if (arr[i] > largest) {
       secondLarge = largest;
       largest = arr[i];
-    } else if (arr[i] > secondLarge) {
+    }
+    // if array has duplicate numbers
+    else if (arr[i] > secondLarge && arr[i] !== largest) {
+      // Update second largest only if it's not equal to the largest
       secondLarge = arr[i];
     }
   }
@@ -19,3 +29,11 @@ function secondLargest(arr) {
 let arr = [15, 0, 19, 10, 8, 17, 1, -4, -8];
 
 secondLargest(arr); // should return 17
+
+/**
+ * Corner cases:
+ * If array is empty
+ * if array has less than two elements
+ * if array has negative numbers - working fine with existing code
+ * if array has duplicate numbers
+ * */
