42c0313ae492828637674321e2dfd8f12bbb2362
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-17 11:54:55 +0530

    NamasteDSA #M1L8 Q8 mxn alt 1 0 print.

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // This code prints a square pattern of asterisks (*) in JavaScript.
 // 1
-// 10
-// 101
+// 01
+// 010
 // 1010
 // 10101
-// 101010
 
-let n = 6;
+let n = 5;
+let toggle = 1;
 for (let i = 0; i < n; i++) {
   let row = "";
+
   for (let j = 0; j <= i; j++) {
-    if (j % 2 === 1) {
-      row = row + 0;
+    row = row + toggle;
+    if (toggle === 1) {
+      toggle = 0;
     } else {
-      row = row + 1;
+      toggle = 1;
     }
   }
   console.log(row);
