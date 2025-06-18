693736f2129df94c3298d03a5f8980db2620a5b4
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-18 18:05:24 +0530

    NamasteDSA #M1L11 Q1 LC-Q7 Reverse Integer.

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-//Given an integer x, return true if x is a palindrome, and false otherwise.
+//Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
-var isPalindrome = function (x) {
-  if (x < 0) {
-    return false;
-  }
-  let rev = 0;
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
   let xCopy = x;
+  x = Math.abs(x);
+
+  let rev = 0;
 
   while (x > 0) {
-    let rem = x % 10;
-    rev = 10 * rev + rem;
+    last = x % 10;
+    rev = 10 * rev + last;
+
     x = Math.floor(x / 10);
   }
 
-  return rev === xCopy;
+  let limit = 2 ** 31;
+  if (rev < -limit || rev > limit - 1) {
+    return 0;
+  }
+
+  return xCopy < 0 ? -rev : rev;
 };
