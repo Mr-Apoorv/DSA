1af25ccb5577eeab4a559078b459e92308606591
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-22 20:36:51 +0530

    NamasteDSA #M3L3 Q1 LC-Q344 Reverse String.

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// 27. Remove Element
+// 344. Reverse String
+// Write a function that reverses a string. The input string is given as an array of characters s.
 
-// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
-
-// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
-
-// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
-// Return k.
+// You must do this by modifying the input array in-place with O(1) extra memory.
 
 /**
- * @param {number[]} nums
- * @param {number} val
- * @return {number}
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
  */
-var removeElement = function (nums, val) {
-  let x = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      nums[x] = nums[i];
-      x++;
-    }
+var reverseString = function (s) {
+  let len = s.length;
+  let halfLen = Math.floor(len / 2);
+  for (let i = 0; i < halfLen; i++) {
+    //swap s[i], s[len - 1 - i]
+    let temp = s[i];
+    s[i] = s[len - 1 - i];
+    s[len - 1 - i] = temp;
   }
-  return x;
 };
