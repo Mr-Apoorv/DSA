0cc600ad9cac89f1c6940ee160e5fcf139e43ad1
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-22 19:52:05 +0530

    NamasteDSA #M3L1 Q2 LC-Q27 Remove element.

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-// LC-26 Remove Duplicates from Sorted Array
-// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// 27. Remove Element
 
-// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
-// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
 // Return k.
 
 /**
  * @param {number[]} nums
+ * @param {number} val
  * @return {number}
  */
-var removeDuplicates = function (nums) {
+var removeElement = function (nums, val) {
   let x = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[x]) {
-      x = x + 1;
+    if (nums[i] !== val) {
       nums[x] = nums[i];
+      x++;
     }
   }
-  return x + 1;
+  return x;
 };
