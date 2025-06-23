46e9055ec15672e853f582e228e97934c655a06b
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-24 02:45:51 +0530

    NamasteDSA #M3L4 Q1 LC-Q121 Best time to buy and sell stock.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-// 344. Reverse String
-// Write a function that reverses a string. The input string is given as an array of characters s.
+// 121. Best Time to Buy and Sell Stock
 
-// You must do this by modifying the input array in-place with O(1) extra memory.
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 /**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
+ * @param {number[]} prices
+ * @return {number}
  */
-var reverseString = function (s) {
-  let len = s.length;
-  let halfLen = Math.floor(len / 2);
-  for (let i = 0; i < halfLen; i++) {
-    //swap s[i], s[len - 1 - i]
-    let temp = s[i];
-    s[i] = s[len - 1 - i];
-    s[len - 1 - i] = temp;
+var maxProfit = function (prices) {
+  let min = prices[0];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] - min > maxProfit) {
+      maxProfit = prices[i] - min;
+    }
+    if (prices[i] < min) {
+      min = prices[i];
+    }
   }
+  return maxProfit;
 };
