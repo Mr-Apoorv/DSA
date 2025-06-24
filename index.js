6e1422d7461e619bb3c3933f2c967844179222ba
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-24 22:19:11 +0530

    NamasteDSA #M3L5 Q1 LC-Q88 Approch-2 Merge Sorted Arrays space-->O(n).

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// 121. Best Time to Buy and Sell Stock
+// 88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
-// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-
-// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 /**
- * @param {number[]} prices
- * @return {number}
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var maxProfit = function (prices) {
-  let min = prices[0];
-  let maxProfit = 0;
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] - min > maxProfit) {
-      maxProfit = prices[i] - min;
-    }
-    if (prices[i] < min) {
-      min = prices[i];
+var merge = function (nums1, m, nums2, n) {
+  let nums1Copy = nums1.slice(0, m);
+  let p1 = 0;
+  let p2 = 0;
+
+  for (let i = 0; i < m + n; i++) {
+    if ((nums1Copy[p1] < nums2[p2] && p1 < m) || p2 >= n) {
+      nums1[i] = nums1Copy[p1];
+      p1++;
+    } else {
+      nums1[i] = nums2[p2];
+      p2++;
     }
   }
-  return maxProfit;
 };
