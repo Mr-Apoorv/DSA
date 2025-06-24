671669e3736cf33fae94d50acfee429904b1d23f
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-24 22:25:23 +0530

    NamasteDSA #M3L5 Q1 LC-Q88 Approch-3 Merge Sorted Arrays space-->O(1).

--- index.js
+++ index.js
@@ -13,17 +13,19 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  let nums1Copy = nums1.slice(0, m);
-  let p1 = 0;
-  let p2 = 0;
+  let p1 = m - 1;
+  let p2 = n - 1;
 
-  for (let i = 0; i < m + n; i++) {
-    if ((nums1Copy[p1] < nums2[p2] && p1 < m) || p2 >= n) {
-      nums1[i] = nums1Copy[p1];
-      p1++;
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
+    if (nums1[p1] > nums2[p2] && p1 >= 0) {
+      nums1[i] = nums1[p1];
+      p1--;
     } else {
       nums1[i] = nums2[p2];
-      p2++;
+      p2--;
     }
   }
 };
