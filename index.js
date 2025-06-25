04177bf41e29656c26b32f88677b671fcd7d1179
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-25 17:04:41 +0530

    NamasteDSA #M3L6 Q1 LC-Q283 Move zeroes.

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-// 88. Merge Sorted Array
-// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+// 283. Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// Note that you must do this in-place without making a copy of the array.
 
-// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
 
 /**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {
-  let p1 = m - 1;
-  let p2 = n - 1;
+var moveZeroes = function (nums) {
+  let x = 0;
 
-  for (let i = m + n - 1; i >= 0; i--) {
-    if (p2 < 0) {
-      break;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[x] = nums[i];
+      x++;
     }
-    if (nums1[p1] > nums2[p2] && p1 >= 0) {
-      nums1[i] = nums1[p1];
-      p1--;
-    } else {
-      nums1[i] = nums2[p2];
-      p2--;
-    }
+  }
+
+  for (let i = x; i < nums.length; i++) {
+    nums[i] = 0;
   }
 };
