54eb40594ded1085310c4dd265aced0f993dd6ce
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-26 20:30:29 +0530

    NamasteDSA #M3L7 Q1 LC-Q485 Max Consecutive ones.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// 283. Move Zeroes
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-// Note that you must do this in-place without making a copy of the array.
+// 485. Max Consecutive Ones
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
 // Example 1:
 
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
 
 /**
  * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
+ * @return {number}
  */
-var moveZeroes = function (nums) {
-  let x = 0;
+var findMaxConsecutiveOnes = function (nums) {
+  let currCount = 0;
+  let maxCount = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[x] = nums[i];
-      x++;
+    if (nums[i] === 1) {
+      currCount++;
+    } else {
+      maxCount = Math.max(currCount, maxCount);
+      currCount = 0;
     }
   }
-
-  for (let i = x; i < nums.length; i++) {
-    nums[i] = 0;
-  }
+  return Math.max(currCount, maxCount);
 };
