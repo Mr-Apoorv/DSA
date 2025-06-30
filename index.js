51f808bf4b7313a7aecd1d286bc5b21ba5a57f10
Apoorv <dixit.a.apoorv@gmail.com>
2025-06-30 21:50:25 +0530

    NamasteDSA #M3L9 Q1 LC-Q136 Single Number - Solution with help of Bitwise XOR. Space --> O(1).

--- index.js
+++ index.js
@@ -14,17 +14,9 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-  let obj = {};
+  let xor = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (!obj[nums[i]]) {
-      obj[nums[i]] = 1;
-    } else {
-      obj[nums[i]] = obj[nums[i]] + 1;
-    }
+    xor = xor ^ nums[i];
   }
-  for (let i = 0; i < nums.length; i++) {
-    if (obj[nums[i]] === 1) {
-      return nums[i];
-    }
-  }
+  return xor;
 };
