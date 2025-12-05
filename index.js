0e1e552e03765e8f9c516399b7fb9b3518f5227b
Apoorv <dixit.a.apoorv@gmail.com>
2025-12-05 20:26:54 +0530

    NamasteDSA #M4L5 Q1 LC-Q509 Fibunacci number.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-// 231. Power of Two
+// 509. Fibonacci Number
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
 
-// Given an integer n, return true if it is a power of two. Otherwise, return false.
-
-// An integer n is a power of two, if there exists an integer x such that n == 2x.
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
 
 /**
  * @param {number} n
- * @return {boolean}
+ * @return {number}
  */
-var isPowerOfTwo = function (n) {
-  if (n === 1) return true;
-  if (n % 2 !== 0 || n < 1) return false;
-
-  return isPowerOfTwo(n / 2);
+var fib = function (n) {
+  if (n <= 1) return n;
+  return fib(n - 1) + fib(n - 2);
 };
+
+let n = 5;
+let value = fib(n);
+
+console.log(value); // Output: 5
