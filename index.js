ddcb2f629743ce26784a930c6494cc07ce8d0f26
Apoorv <dixit.a.apoorv@gmail.com>
2025-12-05 22:02:45 +0530

    NamasteDSA #M5L1 Linear search.

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// 509. Fibonacci Number
-// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+//Linear search
 
-// F(0) = 0, F(1) = 1
-// F(n) = F(n - 1) + F(n - 2), for n > 1.
-// Given n, calculate F(n).
+let array = [4, 9, 2, 0, 10];
 
-/**
- * @param {number} n
- * @return {number}
- */
-var fib = function (n) {
-  if (n <= 1) return n;
-  return fib(n - 1) + fib(n - 2);
-};
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
 
-let n = 5;
-let value = fib(n);
-
-console.log(value); // Output: 5
+console.log(linearSearch(array, 0));
