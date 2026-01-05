cb22c66ccb5e883a91c3cf74dea71a933ff997d2
Apoorv <dixit.a.apoorv@gmail.com>
2026-01-05 16:38:15 +0530

    NamasteDSA #M5L6 Q1 LC-Q912 Merge sort.

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-//insertion sort
+//912. Sort an Array - Merge Sort
 
-let array = [4, 9, -10, 2, 0, 10];
+//Given an array of integers nums, sort the array in ascending order and return it.
 
-function insertionSort(arr) {
-  let n = arr.length;
-  for (let i = 1; i < n; i++) {
-    let curr = arr[i];
-    let prev = i - 1;
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 
-    while (arr[prev] > curr && prev >= 0) {
-      arr[prev + 1] = arr[prev];
-      prev--;
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortArray = function (arr) {
+  if (arr.length <= 1) return arr;
+
+  let mid = Math.floor(arr.length / 2);
+  let left = sortArray(arr.slice(0, mid));
+  let right = sortArray(arr.slice(mid));
+
+  return merge(left, right);
+};
+
+function merge(left, right) {
+  let res = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
     }
-    arr[prev + 1] = curr;
   }
-  return arr;
+  return [...res, ...left.slice(i), ...right.slice(j)];
 }
-
-console.log(insertionSort(array));
